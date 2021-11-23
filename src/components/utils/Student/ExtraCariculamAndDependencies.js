d98656081d393681c0472d05ef8dc22cedb2fc9d
aseelkp <aseelaroor10@gmail.com>
2021-11-23 23:01:59 +0530

    Dependencies completed

--- src/components/utils/Student/ExtraCariculamAndDependencies.js
+++ src/components/utils/Student/ExtraCariculamAndDependencies.js
@@ -33,12 +33,34 @@ const ProfileCard = styled(Card)(({ theme }) => ({
 // const ugCourses = ["Bsc. Computer Science", "B.com", "BBA"]
 
 export default function DependenciesInpu() {
-    // sslc
+    // extra cariculam
     const [activity, setActivity] = useState();
     const [prize , setPrize] = useState();
     const [yearOfParticipation, setYearOfParticipation] = useState();
     const [perfomance , setPerfomance] = useState();
+ 
+    // dependencies
+    const [classRoomPercent , setClassRoomPercent] = useState();
+    const [classRoomRemark , setClassRoomRemark] = useState();
+    const [lectureNotePercent , setLectureNotePercent] = useState();
+    const [lectureNoteRemark , setLectureNoteRemark] = useState();
+    const [noteByTeacherPercent , setNoteByTeacherPercent] = useState();
+    const [noteByTeacherRemark , setNoteByTeacherRemark] = useState();
+    const [caseStudyPercent , setCaseStudyPercent] = useState();
+    const [caseStudyRemark , setCaseStudyRemark] = useState();
+    const [observationPercent , setObservationPercent] = useState();
+    const [observationRemark , setObservationRemark] = useState();
+    const [surveyPercent , setSurveyPercent] = useState();
+    const [surveyRemark , setSurveyRemark] = useState();
+    const [experimentPercent , setExperimentPercent] = useState();
+    const [experimentRemark , setExperimentRemark] = useState();
+    const [extraReadingPercent , setExtraReadingPercent] = useState();
+    const [extraReadingRemark , setExtraReadingRemark] = useState();
+    const [internetPercent , setInternetPercent] = useState();
+    const [internetRemark , setInternetRemark] = useState();
 
+    
+    
 
 
 
@@ -66,7 +88,85 @@ export default function DependenciesInpu() {
 
         {/* Dependencies  */}
         <Grid component={ProfileCard} sx={{ mt: 2, p: 2 }} container spacing={2}>
+
+            <Grid item md={12}><Typography variant="subtitle1">Learing Method and Dependance on each Method</Typography></Grid>
+            <Grid item md={4}><Typography variant="subtitle1">Learning method</Typography></Grid>
+            <Grid item md={4}><Typography variant="subtitle1">% of Dependance</Typography></Grid>
+            <Grid item md={4}><Typography variant="subtitle1">Remark</Typography></Grid>
+            <Grid item md={4}><Typography variant="subtitle1">Class room lecture</Typography></Grid>
+                <Grid item xs={12} sm={6} md={4} lg={4}>
+                    <TextInput label="in %" name="" type="number" textValue={classRoomPercent} setTextValue={setClassRoomPercent} />
+                </Grid>
+                <Grid item xs={12} sm={12} md={4} lg={4} >
+                    <TextInput label="Remark" name="" textValue={classRoomRemark} setTextValue={setClassRoomRemark}  />
+                </Grid>
+            <Grid item md={4}><Typography variant="subtitle1">Lecture Note</Typography></Grid>
+
+                <Grid item xs={12} sm={6} md={4} lg={4}>
+                    <TextInput label="in %" name="" type="number" textValue={lectureNotePercent} setTextValue={setLectureNotePercent} />
+                </Grid>
+                <Grid item xs={12} sm={12} md={4} lg={4} >
+                    <TextInput label="Remark" name="" textValue={lectureNoteRemark} setTextValue={setLectureNoteRemark}  />
+                </Grid>
+            <Grid item md={4}><Typography variant="subtitle1">Note provided by Teacher</Typography></Grid>
+
+                <Grid item xs={12} sm={6} md={4} lg={4}>
+                    <TextInput label="in %" name="" type="number" textValue={noteByTeacherPercent} setTextValue={setNoteByTeacherPercent} />
+                </Grid>
+                <Grid item xs={12} sm={12} md={4} lg={4} >
+                    <TextInput label="Remark" name="" textValue={noteByTeacherRemark} setTextValue={setNoteByTeacherRemark}  />
+                </Grid>
+            <Grid item md={4}><Typography variant="subtitle1">Case Study</Typography></Grid>
+
+                <Grid item xs={12} sm={6} md={4} lg={4}>
+                    <TextInput label="in %" name="" type="number" textValue={caseStudyPercent} setTextValue={setCaseStudyPercent} />
+                </Grid>
+                <Grid item xs={12} sm={12} md={4} lg={4} >
+                    <TextInput label="Remark" name="" textValue={caseStudyRemark} setTextValue={setCaseStudyRemark}  />
+                </Grid>
+            <Grid item md={4}><Typography variant="subtitle1">Observation</Typography></Grid>
+
+                <Grid item xs={12} sm={6} md={4} lg={4}>
+                    <TextInput label="in %" name="" type="number" textValue={observationPercent} setTextValue={setObservationPercent} />
+                </Grid>
+                <Grid item xs={12} sm={12} md={4} lg={4} >
+                    <TextInput label="Remark" name="" textValue={observationRemark} setTextValue={setObservationRemark}  />
+                </Grid>
             
+            <Grid item md={4}><Typography variant="subtitle1">Survey</Typography></Grid>
+
+                <Grid item xs={12} sm={6} md={4} lg={4}>
+                    <TextInput label="in %" name="" type="number" textValue={surveyPercent} setTextValue={setSurveyPercent} />
+                </Grid>
+                <Grid item xs={12} sm={12} md={4} lg={4} >
+                    <TextInput label="Remark" name="" textValue={surveyRemark} setTextValue={setSurveyRemark}  />
+                </Grid>
+            <Grid item md={4}><Typography variant="subtitle1">Experiments</Typography></Grid>
+
+                <Grid item xs={12} sm={6} md={4} lg={4}>
+                    <TextInput label="in %" name="" type="number" textValue={experimentPercent} setTextValue={setExperimentPercent} />
+                </Grid>
+                <Grid item xs={12} sm={12} md={4} lg={4} >
+                    <TextInput label="Remark" name="" textValue={experimentRemark} setTextValue={setExperimentRemark}  />
+                </Grid>
+            <Grid item md={4}><Typography variant="subtitle1">Extra Reading</Typography></Grid>
+
+                <Grid item xs={12} sm={6} md={4} lg={4}>
+                    <TextInput label="in %" name="" type="number" textValue={extraReadingPercent} setTextValue={setExtraReadingPercent} />
+                </Grid>
+                <Grid item xs={12} sm={12} md={4} lg={4} >
+                    <TextInput label="Remark" name="" textValue={extraReadingRemark} setTextValue={setExtraReadingRemark}  />
+                </Grid>
+
+            <Grid item md={4}><Typography variant="subtitle1">Internet/Learing App</Typography></Grid>
+
+                <Grid item xs={12} sm={6} md={4} lg={4}>
+                    <TextInput label="in %" name="" type="number" textValue={internetPercent} setTextValue={setInternetPercent} />
+                </Grid>
+                <Grid item xs={12} sm={12} md={4} lg={4} >
+                    <TextInput label="Remark" name="" textValue={internetRemark} setTextValue={setInternetRemark}  />
+                </Grid>
+
         </Grid>
         
 
